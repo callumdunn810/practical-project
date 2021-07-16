7cd658154e563b633314bef31648d39939dddc32
Callum Dunn <callumdunn810@gmail.com>
2021-07-16 16:08:24 +0000

    added service4-race

---
+++ service4-race/test_race/test_race.py
@@ -0,0 +1,17 @@
+from flask import url_for
+from flask_testing import TestCase
+import requests_mock
+from unittest.mock import patch
+from application import app
+
+
+
+class TestBase(TestCase):
+    def create_app(self):
+        return app
+
+
+class TestHome(TestBase):
+    def test_race(self):
+        response = self.client.post(url_for('get_race'), data='Aldmeri Dominion')
+        self.assertEqual(response.data.decode("utf-8"),'High Elf')
