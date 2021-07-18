241a9c4971cfd0578693b4c610f86a270dc407f6
callumdunn810 <84901668+callumdunn810@users.noreply.github.com>
2021-07-18 14:21:06 +0100

    Delete service2-skill directory

--- service2-skill/application/routes.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, request
-import random
-from application import app
-
-@app.route('/get_skill', methods=['GET'])
-def get_skill():
-    return random.choice(['Necromancer','Nightblade','Sorcerer','Templar','Warden','Dragonknight'])
-
