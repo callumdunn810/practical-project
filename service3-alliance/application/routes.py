15d08e918cd27f738d0fee5e0444c333a2f7c949
callumdunn810 <84901668+callumdunn810@users.noreply.github.com>
2021-07-18 14:18:10 +0100

    Delete service3-alliance directory

--- service3-alliance/application/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, request
-import random
-from application import app
-
-@app.route('/get_alliance', methods=['GET'])
-def get_alliance():
-    return random.choice(['Aldmeri Dominion','Daggerfall Covenant','Ebonheart Pact'])
